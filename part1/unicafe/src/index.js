c871c9d291c2b4e7dd60451c9ce373d0bdea76b2
SimonArcher <simonnline@gmail.com>
2023-05-03 11:25:27 +0700

    unicafe step1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,6 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
-const counter = 0
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)

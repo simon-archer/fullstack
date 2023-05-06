e8f6ba242b3ab928dbe6a79937e2e76db675903a
SimonArcher <simonnline@gmail.com>
2023-05-06 15:10:07 +0700

    fixed: course information step5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,15 +1,53 @@
-import { useState } from 'react'
+const course = {
+  name: 'Half Stack application development',
+  parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+}
+
+const Header = (props) => {
+  return (
+  <h1>{props.course.name}</h1>
+  )
+}
+
+const Content = (props) => {  
+    return( 
+      <>
+        <p>{props.parts[0].name}</p>
+        <p>{props.parts[1].name}</p>
+        <p>{props.parts[2].name}</p>
+      </>
+    )
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </>
+  )
+}
+
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
-        plus
-      </button>
+      <Header course={course}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }

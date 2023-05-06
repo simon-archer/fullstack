dcd2cff98396274d3b1976e9f0fd1f977ae91f3c
SimonArcher <simonnline@gmail.com>
2023-05-06 14:14:46 +0700

    unicafe part6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,73 +1,86 @@
+import logo from './logo.svg';
+import './App.css';
+
 import { useState } from 'react'
 
-const GetAnecdote = ({anecdotes, selected}) => {
+const StatisticsLine = (props) => {
+  const {text, value} = props
+  // const total = good + neutral + bad
+  // const average = total / 3
+  // const positivePercentage = good / total
+  return (
+    <tbody>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+    </tbody>
+  )
+}
+
+const Statistics = (props) => {
+  const {good, neutral, bad} = props
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positivePercentage = good / total * 100 + '%'
+if (total === 0) {
+    return (
+      <div>
+      <h1>statistics</h1>
+      <p>No feedback given</p>
+      </div>
+    )
+  } {   
+  return(
+    <div>
+      <h1>statistics</h1>
+      <table>
+      <StatisticsLine text="good" value ={good} />
+      <StatisticsLine text="neutral" value ={neutral} />
+      <StatisticsLine text="bad" value ={bad} />
+      <StatisticsLine text="total" value ={total} />
+      <StatisticsLine text="average" value ={average} />
+      <StatisticsLine text="positive" value ={positivePercentage} />
+      </table>
+    </div>
+  )
+  }
+}
+
+const Button = (props) => {
+  const {text, handler} = props
   return (
     <>
-      <p>{anecdotes}</p>
+    <button onClick={handler}>{text}</button>
     </>
   )
 }
 
-
-// const GetAnecdote = (props) => {
-//   const [anecdotes, selected] = props
-//   return (
-//     <>
-//       <p>{anecdotes[selected]}</p>
-//     </>
-//   )
-// }
-const VoteClick = ({votes, incrementVote, selected}) => {
-  return (
-    <>
-      <p>Has {votes[selected]} votes</p>
-      <button onClick={incrementVote}>Vote</button>
-    </>
-    )
-}
-
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const handleClick = () => {
-    const randomIndex = Math.floor(Math.random() * anecdotes.length)
-    setSelected(randomIndex)
+  const handleGoodClick = () => {
+    setGood(good + 1)
   }
 
-  const incrementVote = (props) => {
-    const newVotes = [... votes]
-    newVotes[selected] += 1
-    setVotes(newVotes)
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
   }
 
-  const mostVotedIndex = () => {
-    if (Math.max(...votes) === 0) {
-      return <p>No votes yet</p>;
-    } else {
-      return votes.indexOf(Math.max(...votes))
-    }
+  const handleBadClick = () => {
+    setBad(bad + 1)
   }
 
   return (
     <div>
-      <h1>Anecdote of the day</h1>
-      <GetAnecdote anecdotes={anecdotes[selected]}/>
-      <VoteClick votes={votes} incrementVote={incrementVote} selected={selected} />
-      <button onClick={handleClick}>Next Anecdote</button>
-      <h1>Anecdote with most votes</h1>
-      <GetAnecdote anecdotes={anecdotes[mostVotedIndex()]} />
+      <h1>give feedback</h1>
+      <Button text="good" handler={handleGoodClick} />
+      <Button text="neutral" handler={handleNeutralClick} />
+      <Button text="bad" handler={handleBadClick} />
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }

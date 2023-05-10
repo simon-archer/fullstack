2d9f4c471e89c1c586b230af81e172c3db95c55f
SimonArcher <simonnline@gmail.com>
2023-05-10 11:31:00 +0700

    Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,44 +2,72 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
-    { name: 'Ada Lovelace'}
+    { 
+      name: 'Arto Hellas', 
+      number: '040-1234567'
+    },
+    { 
+      name: 'Ada Lovelace',
+      number: '47-47474747'
+    }
   ]) 
 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const handleInputChange = (event) => {
+  const handleNameInput = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
+  const handleNumberInput = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+
   const addContact = (event) => {
     event.preventDefault()
-    const contactObject = {
-      name: newName
-    }
+    const nameExists = newName ? persons.some(person => person.name === newName) : false;
 
+    if (nameExists){
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      const contactObject = {
+        name: newName,
+        number: newNumber
+      }
     setPersons(persons.concat(contactObject))
     setNewName('')
+    setNewNumber('')
+    }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addContact}>
         <div>
           Name: <input 
             value={newName}
-            onChange={handleInputChange}
+            onChange={handleNameInput}
           />
+          
         </div>
         <div>
-          <button type="submit" onClick={addContact}>Add</button>
+          Number: <input 
+            value={newNumber}
+            onChange={handleNumberInput}
+          />
+          
+        </div>
+        <div>
+          <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map ((person, index) => 
-        <p key={index}>{person.name}</p>
+        <p key={index}>{person.name + ' ' + person.number}</p>
       )}
     </div>
   )

4beaf9d914a480b6f0fb1eb980d1cb7a5bcfc291
SimonArcher <simonnline@gmail.com>
2023-05-11 11:54:15 +0700

    Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({filterSearch, setFilterSearch}) => {
 
@@ -86,13 +87,19 @@ const People = ({ people }) => {
 
 const App = () => {
   const [filterSearch, setFilterSearch] = useState("")
+  const [people, setPeople] = useState([])
 
-  const [people, setPeople] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/people')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPeople(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
   const sortedPeople = () => {
     if (!people) return []

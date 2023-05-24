68f856fdda73e173ce0eaa32bbfa7655c7ee4420
SimonArcher <simonnline@gmail.com>
2023-05-24 10:18:38 +0300

    4.5*: helper functions and unit tests, step3

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -1,32 +1,47 @@
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-    const blogs = []
+const blogs = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Statement',
+        author: 'Edsger ',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 4,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Harmful',
+        author: ' W.',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 4,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go',
+        author: 'Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 12,
+        __v: 0
+    }
+]
 
+test('dummy returns one', () => {
     const result = listHelper.dummy(blogs)
     expect(result).toBe(1)
 })
 
 test('Upvotes should return 8', () => {
-    const blogs = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 4,
-            __v: 0
-        },
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 4,
-            __v: 0
-        }
-    ]
-
     const result = listHelper.totalLikes(blogs)
-    expect(result).toBe(8)
+    expect(result).toBe(20)
+})
+
+test('Should return title: Go, Author: Dijkstra, Likes: 12', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual({
+        title: 'Go',
+        author: 'Dijkstra',
+        likes: 12
+    })
 })
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,3 +1,5 @@
+const blog = require('../models/blog')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -6,7 +8,18 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const favorite = blogs.reduce((max, blog) =>
+        max.likes > blog.likes ? max : blog, blogs[0])
+    return {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }

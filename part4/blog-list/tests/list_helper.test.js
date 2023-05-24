90a1ccf87cb0bdd048d30b4c6284eb8df20518ca
SimonArcher <simonnline@gmail.com>
2023-05-24 09:52:23 +0300

    helper functions and unit tests, step2

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -6,3 +6,27 @@ test('dummy returns one', () => {
     const result = listHelper.dummy(blogs)
     expect(result).toBe(1)
 })
+
+test('Upvotes should return 8', () => {
+    const blogs = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 4,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 4,
+            __v: 0
+        }
+    ]
+
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(8)
+})

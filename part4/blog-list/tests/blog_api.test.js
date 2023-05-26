eb3d1590cdef4d98d8b65c4a4836a16341e6dad2
SimonArcher <simonnline@gmail.com>
2023-05-26 16:56:24 +0300

    4.10: Blog list tests, step3

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-const blog = require('../models/blog')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+beforeEach(async () => {
+    await Blog.deleteMany({});
+});
 
 test('All blogs are returned as JSON', async () => {
     await api
@@ -11,19 +15,39 @@ test('All blogs are returned as JSON', async () => {
         .expect(200)
         .expect('Content-Type', /application\/json/)
         .then(response => {
-            expect(response.body.length).toBe(5)
+            expect(response.body.length).toBe(0)
         })
 
 })
 
 test('The id has been generated', async () => {
+    const payload = {
+        'title': 'Brisket',
+        'author': 'Zuccy',
+        'url': 'https://www.meta.com',
+        'likes': 3
+    }
     const response = await api
-        .get('/api/blogs')
-        .expect(200)
-    response.body.forEach(blog => {
-        expect(blog.id).toBeDefined()
-    })
+        .post('/api/blogs')
+        .send(payload)
+        .set('Content-Type', 'application/json/')
+        .expect(201)
 
+    expect(response.body.id).toBeDefined()
+})
+
+test('Created a new blog post', async () => {
+    const payload = {
+        'title': 'Brisket',
+        'author': 'Zuccy',
+        'url': 'https://www.meta.com',
+        'likes': 3
+    }
+    await api
+        .post('/api/blogs')
+        .send(payload)
+        .set('Content-Type', 'application/json/')
+        .expect(201)
 })
 
 afterAll(async () => {

23a44b39c25517c7b52f79282556ca502807b08e
SimonArcher <simonnline@gmail.com>
2023-05-26 13:08:16 +0300

    Pre-excercise cleanup

--- part3/notes-backend/.eslintrc.js
+++ part3/notes-backend/.eslintrc.js
@@ -2,21 +2,40 @@ module.exports = {
     'env': {
         'node': true,
         'commonjs': true,
-        'es2021': true
+        'es2021': true,
+        'jest': true,
     },
     'extends': 'eslint:recommended',
+    'overrides': [
+    ],
     'parserOptions': {
         'ecmaVersion': 'latest'
     },
     'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
         'eqeqeq': 'error',
         'no-trailing-spaces': 'error',
-        'object-curly-spacing': ['error', 'always'],
-        'arrow-spacing': ['error', { 'before': true, 'after': true }],
+        'object-curly-spacing': [
+            'error', 'always'
+        ],
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true }
+        ],
         'no-console': 0,
-        'indent': ['error', 4],
-        'linebreak-style': ['error', 'unix'],
-        'quotes': ['error', 'single'],
-        'semi': ['error', 'never']
     }
 }

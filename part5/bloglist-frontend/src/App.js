cc5999d20aac93f81a76f5c8a00f12e7dd56f9de
SimonArcher <simonnline@gmail.com>
2023-06-03 17:27:01 +0200

    5.2: bloglist frontend, step2

---
+++ part5/bloglist-frontend/src/App.js
@@ -0,0 +1,108 @@
+import { useState, useEffect } from 'react'
+
+import Blog from './components/Blog'
+import Notification from './components/Notification'
+
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const handleLogout = async (event) => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
+
+  // const handleLogout = async (event) => {
+  //   event.preventDefault()
+  //   window.localStorage.removeItem('loggedNoteappUser')
+  //   setUser(null)
+  // }
+
+  useEffect(() => {
+    if (user) {
+      blogService.getAll().then(blogs =>
+        setBlogs(blogs)
+      )
+    }
+  }, [user])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  return (
+    <div>
+      <h2>Blogs</h2>
+      <Notification message={errorMessage} />
+      {!user && loginForm()}
+      {user && <div>
+        <p>{user.name} logged in</p>
+        <button onClick={handleLogout}>Log out</button>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+      }
+    </div>
+  )
+}
+
+export default App
---
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+const baseUrl = '/api/blogs'
+
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
+const getAll = () => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const request = axios.get(baseUrl, config)
+  return request.then(response => response.data)
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { setToken, getAll }
+
+// export default { getAll, setToken }
+
+
+// import axios from 'axios'
+// const baseUrl = '/api/notes'
+
+// let token = null
+
+// const setToken = newToken => {
+//   token = `Bearer ${newToken}`
+// }
+
+// const getAll = () => {
+//   const request = axios.get(baseUrl)
+//   return request.then(response => response.data)
+// }
+
+// const create = async newObject => {
+//   const config = {
+//     headers: { Authorization: token },
+//   }
+//   const response = await axios.post(baseUrl, newObject, config)
+//   return response.data
+// }
+
+// const update = (id, newObject) => {
+//   const request = axios.put(`${baseUrl} /${id}`, newObject)
+//   return request.then(response => response.data)
+// }
+
+// eslint-disable-next-line import/no-anonymous-default-export

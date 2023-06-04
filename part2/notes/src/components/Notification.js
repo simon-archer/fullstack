2d7977bacfb3625e1a8d6338a0df0bf85e10cd91
SimonArcher <simonnline@gmail.com>
2023-06-04 08:44:22 +0200

    Fixed capitalisation error for Notification.js

--- part2/notes/src/components/Notification.js
+++ part2/notes/src/components/Notification.js
@@ -1,13 +1,13 @@
 const Notification = ({ message }) => {
-    if (message === null) {
-      return null
-    }
-  
-    return (
-      <div className="userMessage">
-        {message}
-      </div>
-    )
+  if (message === null) {
+    return null
   }
-  
-  export default Notification
+
+  return (
+    <div className="userMessage">
+      {message}
+    </div>
+  )
+}
+
+export default Notification
